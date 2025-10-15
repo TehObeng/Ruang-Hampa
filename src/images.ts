54d3094d59595db57b2cb3457309ee69784896ac
Danel Smile <smdanel321@gmail.com>
2025-10-15 08:33:06 +0700

    Images

--- src/images.ts
+++ src/images.ts
@@ -1,14 +1,14 @@
 // Import each image file directly into the script.
 // This allows the Vite build tool to process the images, assign them a unique hashed filename,
 // and return the correct public URL for use in the application.
-import banyuRoomMorning from './img/banyu_room_morning.jpg';
-import banyuRoomStayInBed from './img/banyu_room_stay_in_bed.jpg';
-import phoneScreen from './img/phone_screen.jpg';
-import kitchenMorning from './img/kitchen_morning.jpg';
-import ruangMakan from './img/ruang_makan.jpg';
-import kampus from './img/kampus.jpg';
-import jembatanMalam from './img/jembatan_malam.jpg';
-import terminalBus from './img/terminal_bus.jpg';
+import banyuRoomMorning from '../img/banyu_room_morning.jpg';
+import banyuRoomStayInBed from '../img/banyu_room_stay_in_bed.jpg';
+import phoneScreen from '../img/phone_screen.jpg';
+import kitchenMorning from '../img/kitchen_morning.jpg';
+import ruangMakan from '../img/ruang_makan.jpg';
+import kampus from '../img/kampus.jpg';
+import jembatanMalam from '../img/jembatan_malam.jpg';
+import terminalBus from '../img/terminal_bus.jpg';
 
 // The images object now maps the story keys to the URLs provided by the import statements.
 export const images: { [key: string]: string } = {
